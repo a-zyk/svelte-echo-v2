0e2fdd9ea3f0619f2d301d4edc354618a7f4b45e
a-zyk <austejazykute@gmail.com>
2024-02-12 15:16:16 +0200

    add collapse to form elements

--- src/lib/PolicyInput.svelte
+++ src/lib/PolicyInput.svelte
@@ -4,21 +4,26 @@
 </script>
 
 <div class="flex flex-col card card-compact bg-base-100 shadow-xl m-4">
-  <div class="card-body">
-    <div class="self-center mb-4 card-title">{item.title}</div>
+  <div  class="card-body collapse collapse-arrow">
+    <input type="checkbox" class="peer" checked />
+    <div class="self-center mb-4 card-title collapse-title">{item.title}</div>
     {#if item.groups}
+    <div class="collapse-content">
+
       {#each item.groups as group}
-        <form>
+        <form >
           <div class="flex flex-col gap-4">
-            <div class="self-center">{group.title}</div>
+            <div class="self-center ">{group.title}</div>
             <Inputs prefix={group.name} inputs={item.inputs} />
           </div>
         </form>
       {/each}
+    </div>
     {:else}
-      <form>
+      <form class="collapse-content">
         <Inputs inputs={item.inputs} prefix={item.name} />
       </form>
     {/if}
   </div>
 </div>
+
